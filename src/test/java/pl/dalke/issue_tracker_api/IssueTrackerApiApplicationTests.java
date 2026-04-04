72c09bcf8719b6d748a68f081a5c222d2a116bb3
Mateusz Dalke <sculpttechproject@gmail.com>
2026-04-04 13:18:37 +0200

    chore: bootstrap spring app with postgres

---
+++ src/test/java/pl/dalke/issue_tracker_api/IssueTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dalke.issue_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
