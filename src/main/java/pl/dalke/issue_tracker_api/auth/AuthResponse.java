56958367e6465f1c8551cccf48eec1dda2b1aea6
SculptTechProject <150788324+SculptTechProject@users.noreply.github.com>
2026-04-05 21:17:51 +0200

    add auth package

---
+++ src/main/java/pl/dalke/issue_tracker_api/auth/AuthResponse.java
@@ -0,0 +1,4 @@
+package pl.dalke.issue_tracker_api.auth;
+
+public class AuthResponse {
+}
--- src/main/java/pl/dalke/issue_tracker_api/user/UserController.java
+++ src/main/java/pl/dalke/issue_tracker_api/user/UserController.java
@@ -1,8 +1,10 @@
 package pl.dalke.issue_tracker_api.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/api/users")
+@RestController()
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
